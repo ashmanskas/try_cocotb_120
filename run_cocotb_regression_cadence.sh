d487551fdad58fa871465db5e28e13bcfbbec5ae
Bill Ashmanskas <ashmansk@hep.upenn.edu>
2019-07-31 16:05:12 -0400

    commit

--- run_cocotb_regression_cadence.sh
+++ run_cocotb_regression_cadence.sh
@@ -5,6 +5,7 @@ eval "$(/tape/ashmansk/anaconda3/bin/conda shell.bash hook)"
 export PATH=/cad/INCISIVE152-lnx/tools/bin:$PATH
 export ARCH=x86_64
 export SIM=ius
+export LM_LICENSE_FILE=1700@head
 export PYTHON_BIN=python3
 make
 
--- run_cocotb_regression_questa.sh
+++ run_cocotb_regression_questa.sh
@@ -5,6 +5,7 @@ eval "$(/tape/ashmansk/anaconda3/bin/conda shell.bash hook)"
 export PATH=/cad/mentor/questa-10.7/questasim/bin:$PATH
 export ARCH=x86_64
 export SIM=questa
+export LM_LICENSE_FILE=1717@jicama.seas.upenn.edu
 export PYTHON_BIN=python3
 make
 
